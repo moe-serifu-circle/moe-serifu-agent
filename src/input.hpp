99e6d405cb66aea406e86f996ff321eac896e241
Rebecca Nelson <tails.fanatic@gmail.com>
2017-02-28 14:32:45 -0600

    Header file from prev commit

--- src/input.hpp
+++ src/input.hpp
@@ -11,17 +11,17 @@ namespace msa { namespace input {
 
 	typedef enum input_type_type { TTY, TCP, UDP } InputType;
 
-	typedef struct input_chunk_type InputChunk;
+	typedef struct chunk_type Chunk;
 
-	typedef struct input_device_type InputDevice;
+	typedef struct device_type Device;
 
 	extern int init(msa::Handle hdl, const msa::config::Section &config);
 	extern int quit(msa::Handle hdl);
-	extern void add_input_device(msa::Handle hdl, InputType type, void *device_id);
-	extern void get_input_devices(msa::Handle hdl, std::vector<const std::string> *list);
-	extern void remove_input_device(msa::Handle hdl, const std::string &id);
-	extern void enable_input_device(msa::Handle hdl, const std::string &id);
-	extern void disable_input_device(msa::Handle hdl, const std::string &id);
+	extern void add_device(msa::Handle hdl, InputType type, void *device_id);
+	extern void get_devices(msa::Handle hdl, std::vector<const std::string> *list);
+	extern void remove_device(msa::Handle hdl, const std::string &id);
+	extern void enable_device(msa::Handle hdl, const std::string &id);
+	extern void disable_device(msa::Handle hdl, const std::string &id);
 } }
 
 #endif

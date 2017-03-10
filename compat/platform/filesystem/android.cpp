309a1129f9f1edf8b1a1dc6de252c0d01237f03c
Rebecca Nelson <tails.fanatic@gmail.com>
2017-03-10 11:06:01 -0600

    Added filesystem compat module

---
+++ compat/platform/filesystem/android.cpp
@@ -0,0 +1,23 @@
+#include <dirent.h>
+#include <errno.h>
+
+#include <string>
+
+namespace msa { namespace file {
+
+	extern void list(const std::string &dir_path, std::vector<std::string> &files)
+	{
+		DIR *d = opendir(dir_path.c_str());
+		if (d == NULL)
+		{
+			throw std::logic_error("could not open dir (" + std::to_string(errno) + "): " + dir_path);
+		}
+		struct dirent *entry;
+		while ((entry = readdir(d)) != NULL)
+		{
+			files.push_back(std::string(entry->d_name));
+		}
+		closedir(d);
+	}
+
+} }
---
+++ compat/platform/filesystem/filesystem.cpp
@@ -0,0 +1,9 @@
+#include "filesystem.hpp"
+
+#if defined(__WIN32)
+	#include "win32.cpp"
+#elif defined(__ANDROID__)
+	#include "android.cpp"
+#else
+	#include "unix.cpp"
+#endif

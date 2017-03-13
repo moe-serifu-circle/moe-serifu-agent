08e1d5da0df602ad9051d77a293dd44c1b6a163c
Rebecca Nelson <rebecca.nelson@cliftonlabs.com>
2017-03-13 10:20:27 -0500

    Updated interface for plugin types

--- src/plugin/types.hpp
+++ src/plugin/types.hpp
@@ -8,6 +8,7 @@
 
 #include <cstdint>
 #include <vector>
+#include <string>
 
 namespace msa { namespace plugin {
 
@@ -18,35 +19,98 @@ namespace msa { namespace plugin {
 	typedef int (*AddCommandsFunc)(msa::Handle hdl, void *plugin_env, std::vector<msa::cmd::Command *> &new_commands);
 	typedef int (*InitFunc)(msa::Handle hdl, void **plugin_env);
 
+	
 	typedef struct version_type
 	{
+		// some compilers (looking at you, GNU.) clobber the symbols major and minor, which interfers with our
+		// init lists for the version struct. Undefine them for now if that is the case and turn them back on
+		// after the version struct.
+		#ifdef major
+			#define __major_temp major
+			#undef major
+		#endif
+		#ifdef minor
+			#define __minor_temp minor
+			#undef minor
+		#endif
 		uint32_t major;
 		uint32_t minor;
 		uint32_t debug;
 		uint32_t build;
+		version_type() : major(0), minor(0), debug(0), build(0) {}
+		version_type(uint32_t major) : major(major), minor(0), debug(0), build(0) {}
+		version_type(uint32_t major, uint32_t minor) : major(major), minor(minor), debug(0), build(0) {}
+		version_type(uint32_t major, uint32_t minor, uint32_t debug) : major(major), minor(minor), debug(debug), build(0) {}
+		version_type(uint32_t major, uint32_t minor, uint32_t debug, uint32_t release) : major(major), minor(minor), debug(debug), build(release) {}
+		version_type(const std::string &ver_str) : major(0), minor(0), debug(0), build(0)
+		{
+			size_t parse_pos;
+			// parse major
+			std::string ver = ver_str;
+			major = (uint32_t) std::stol(ver, &parse_pos);
+			if (parse_pos >= (ver.size() - 2) || ver[parse_pos] != '.')
+			{
+				return;
+			}
+			// parse minor
+			ver = ver.substr(parse_pos + 1);
+			minor = (uint32_t) std::stol(ver, &parse_pos);
+			if (parse_pos >= (ver.size() - 2) || ver[parse_pos] != '.')
+			{
+				return;
+			}
+			// parse debug
+			ver = ver.substr(parse_pos + 1);
+			debug = (uint32_t) std::stol(ver, &parse_pos);
+			if (parse_pos >= (ver.size() - 2) || ver[parse_pos] != 'b')
+			{
+				return;
+			}
+			// parse build
+			ver = ver.substr(parse_pos + 1);
+			build = (uint32_t) std::stol(ver, &parse_pos);
+		}
+		// we are done using major and minor in the version code, so turn them back on
+		#ifdef __major_temp
+			#define major __major_temp
+			#undef __major_temp
+		#endif
+		#ifdef __minor_temp
+			#define minor __minor_temp
+			#undef __minor_temp
+		#endif
 	} Version;
-
-	struct info_type
+	
+	typedef struct function_table_type
 	{
-		const char *name;
-		std::vector<std::string> authors;
-		Version version;
 		InitFunc init_func;
 		Func quit_func;
 		Func add_input_devices_func;
 		Func add_output_devices_func;
 		Func add_agent_props_func;
 		AddCommandsFunc add_commands_func;
+	} FunctionTable;
+
+	struct info_type
+	{
+		std::string name;
+		std::vector<std::string> authors;
+		Version version;
+		const FunctionTable *functions;
 		info_type() :
 			name(""),
 			authors(),
-			version({0, 0, 0, 0}),
-			init_func(NULL),
-			quit_func(NULL),
-			add_input_devices_func(NULL),
-			add_output_devices_func(NULL),
-			add_agent_props_func(NULL),
-			add_commands_func(NULL)
+			version(0, 0, 0, 0),
+			functions(NULL)
+			{}
+		info_type(const std::string &name,
+					const std::vector<std::string> &authors,
+					const Version version,
+					const FunctionTable *funcs) :
+			name(name),
+			authors(authors),
+			version(version),
+			functions(funcs)
 			{}
 	};
 

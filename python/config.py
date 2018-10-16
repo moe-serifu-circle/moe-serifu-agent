3057f03732309f3f5d8964f8355029524fb97c81
Jpiv1212 <33266861+Jpiv1212@users.noreply.github.com>
2018-10-16 16:02:22 -0500

    Update python/config.py (#35)
    Created class ConfigException

---
+++ python/config.py
@@ -0,0 +1,28 @@
+class ConfigError(Exception):
+    def __init__(self, sec, key, val, msg, index=0):
+        cache = str(sec)+"."+str(key)
+        if index != 0:
+            cache += "[" + str(index) + "]"
+        cache += " \""+str(val)+"\" " + msg
+        super().__init__(cache)
+        self._sec = sec
+        self._key = key
+        self._val = val
+        self._msg = msg
+        self._index = index
+
+    def key():
+        return self._key
+
+    def value():
+        return self._val
+
+    def section():
+        return self._sec
+
+    def message():
+        return self._msg
+
+    def index():
+        return self._index
+        

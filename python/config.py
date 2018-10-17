7c7a9ebbcde9aaf5389901c0862794230e7153f7
Jarod <jpiv12307@gmail.com>
2018-10-16 20:57:56 -0500

    first part of load/save

--- python/config.py
+++ python/config.py
@@ -1,3 +1,37 @@
+import io
+
+COMMENT_CHAR = '#'
+SECTION_HEADER_START_CHAR = '['
+SECTION_HEADER_END_CHAR = ']'
+
+def load(filepath):
+    file = open(filepath, "r")
+    lines = file.readlines()
+    file.close()
+
+    cur_sect = None
+    sections = {}
+    for line in lines:
+        if line[0] == COMMENT_CHAR or len(line.strip()) == 0: #checking for whitespace only
+            pass
+        elif line[0] == SECTION_HEADER_START_CHAR and line.strip()[-1] == SECTION_HEADER_END_CHAR:
+            cur_sect = line[1:-1]
+            if cur_sect not in list(sections.keys()):
+                sections[cur_sect] = Section(cur_sect)
+        elif "=" in line:
+            index = line.index("=")
+            key = line[:index].strip()
+            val = line[index+1:].strip()
+            if "[" in val and val[-1] == "]":
+                starti = (len(val)-1)-val[::-1].index("[") #finds final instance of '[' in line, in case another instance is within val
+                try:
+                    #end of point I can write here
+            
+
+def save(config, filepath):
+    if type(config) != Config:
+        raise TypeError("Can only save instances of Config")
+
 class Config():
     def __init__(self, config_file, sections):
         self.filepath = config_file

db2bc1956e68c5aeb6655f8b0023f2d97dcd8ae5
Jarod <jpiv12307@gmail.com>
2018-10-31 19:28:06 -0500

    Update python/msa/event.py

--- python/msa/event.py
+++ python/msa/event.py
@@ -1,9 +1,17 @@
-from enum import IntEnum  # IntEnum is used to facilitate simple comparisons
+from enum import Enum, auto
 
 
-class Topics(IntEnum):
-    # Greater priority topics interrupt event handling of lower-numbered priority topics.
-    EVENT_STACK_CLEARED = 0
-    EVENT_HANDLED = 0
-    EVENT_INTERRUPTED = 0
-    TEXT_INPUT = 10
+class Topics(Enum):
+    EVENT_STACK_CLEARED = auto()
+    EVENT_HANDLED = auto()
+    EVENT_INTERRUPTED = auto()
+    TEXT_INPUT = auto()
+
+    @property
+    def priority(self):
+        # Greater priority topics interrupt event handling of lower-numbered priority topics.
+        prior_dict = {Topics.EVENT_STACK_CLEARED: 0,
+                      Topics.EVENT_HANDLED: 0,
+                      Topics.EVENT_INTERRUPTED: 0,
+                      Topics.TEXT_INPUT: 10}
+        return prior_dict[self]

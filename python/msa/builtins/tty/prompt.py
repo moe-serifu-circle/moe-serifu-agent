26fafc5a5c555a1f34269cdff6ba3d4dbc4f3dac
JordanGoetze <jordan.goetze@target.com>
2018-11-19 15:12:32 -0600

    Adds comments.

--- python/msa/builtins/tty/prompt.py
+++ python/msa/builtins/tty/prompt.py
@@ -4,7 +4,9 @@ import platform
 
 from msa.core import supervisor
 
+
 class Prompt:
+    """An asyncio based text prompt. Listens for input from a TTY. Used by the TtyInputHandler to get input."""
     def __init__(self, loop):
         self.loop = loop
         self.q = asyncio.Queue(loop = self.loop)
@@ -13,10 +15,12 @@ class Prompt:
             self.loop.add_reader(sys.stdin, self.handle_input)
 
     def handle_input(self):
+        """Callback used by the asyncio loop reader when there is input to stdin"""
         future = self.q.put(sys.stdin.readline())
         asyncio.ensure_future(future, loop=self.loop)
 
     def listen(self, wait=False):
+        """Listen for prompt input"""
 
         if platform.system() == "Windows":
             return asyncio.get_event_loop().run_in_executor(supervisor.executor, sys.stdin.readline)

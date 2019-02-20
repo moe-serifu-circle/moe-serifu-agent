d8d5391f5f237c0db1540b08199ee4e059adcde0
JordanGoetze <jghibiki.games@gmail.com>
2019-02-20 10:17:18 -0600

    rework build all to increase resolution for startup_and_echo_sequence_diagram

--- docs/architecture/rework/build_all.sh
+++ docs/architecture/rework/build_all.sh
@@ -2,9 +2,20 @@
 
 rm *.png
 
-for i in *.mermaid; do
 
-    fn=`basename $i .mermaid`
-    echo "Building $fn.png"
-    mmdc -i $i -o $fn.png
-done
+function build_diagram(){
+    echo "Building $1.png"
+
+    if [[ $2 != "" && $3 != "" ]]; then
+        mmdc -i $1.mermaid -o $1.png -w $2 -H $3
+    else
+        mmdc -i $1.mermaid -o $1.png 
+    fi
+}
+
+build_diagram startup_and_echo_sequence_diagram 1568 3184
+build_diagram user_script_submission
+build_diagram user_script_submission_interactive
+build_diagram user_hook_script_submission
+
+

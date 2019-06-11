fca51b1db93b29aae57e3cd8489383eb1916db86
JordanGoetze <jghibiki.games@gmail.com>
2019-06-11 15:19:00 -0500

    fixes db conn

--- python/msa/builtins/scripting/handlers.py
+++ python/msa/builtins/scripting/handlers.py
@@ -29,7 +29,6 @@ class AddScriptHandler(EventHandler):
         with self.event_bus.subscribe([events.AddScriptEvent]) as queue:
 
             _, event = await queue.get()
-            print(event)
 
             if isinstance(event, events.AddScriptEvent):
                 insert_new_script = ScriptEntity.insert().values(
@@ -39,22 +38,21 @@ class AddScriptHandler(EventHandler):
                 )
 
 
-                conn = await self.database.connect()
-                result = await conn.execute(ScriptEntity.filter(ScriptEntity.name == event.data["name"]).count())
+                with self.database.connect() as conn:
+                    result = await conn.execute(ScriptEntity.filter(ScriptEntity.name == event.data["name"]).count())
 
-                if result == 0:
-                    await conn.execute(insert_new_script)
-                else:
-                    new_event = events.AddScriptFailedEvent()
-                    new_event.init({
-                            "error": f"Failed to add script {event.data['name']}." ,
-                            "description": "A script with this name already exists.",
-                            "description_verbose": "A script with this name already exists.Delete the script with this name then try again",
-                    })
-                    await supervisor.fire_event(
-                        new_event
-                    )
-                    #await conn.close()
+                    if result == 0:
+                        await conn.execute(insert_new_script)
+                    else:
+                        new_event = events.AddScriptFailedEvent()
+                        new_event.init({
+                                "error": f"Failed to add script {event.data['name']}." ,
+                                "description": "A script with this name already exists.",
+                                "description_verbose": "A script with this name already exists.Delete the script with this name then try again",
+                        })
+                        await supervisor.fire_event(
+                            new_event
+                        )
                 
 
 class TriggerScriptRunHandler(EventHandler):

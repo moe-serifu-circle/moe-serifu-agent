88dbd97a034a82490eae7bbbbb964a0b34ff9dc6
JordanGoetze <jghibiki.games@gmail.com>
2019-10-04 14:42:23 -0500

    adds missed test changes

--- python/tests/core/event_test.py
+++ python/tests/core/event_test.py
@@ -75,7 +75,111 @@ class EventTest(unittest.TestCase):
         assert a != b
         assert a == a
 
+    def test_less_than(self):
+
+        high = HighPriority()
+        medium = MediumPriority()
+        low = LowPriority()
+
+        assert not high < medium
+        assert medium < high
+
+        assert not high < low
+        assert low < high
+
+        assert not medium < low
+        assert low < medium
+
+    def test_less_than_equals(self):
+
+        high = HighPriority()
+        high2 = HighPriority()
+        medium = MediumPriority()
+        medium2 = MediumPriority()
+        low = LowPriority()
+        low2 = LowPriority()
+
+        assert high <= high2
+        assert not high <= medium
+        assert medium <= high
+
+        assert medium <= medium2
+        assert not high <= low
+        assert low <= high
+
+        assert low <= low2
+        assert not medium <= low
+        assert low <= medium
         
+    def test_greater_than(self):
+
+        high = HighPriority()
+        medium = MediumPriority()
+        low = LowPriority()
+
+        assert high > medium
+        assert not medium > high
+
+        assert high > low
+        assert not low > high
+
+        assert medium > low
+        assert not low > medium
+
+    def test_greater_than_equals(self):
+
+        high = HighPriority()
+        high2 = HighPriority()
+        medium = MediumPriority()
+        medium2 = MediumPriority()
+        low = LowPriority()
+        low2 = LowPriority()
+
+        assert high >= high2
+        assert high >= medium
+        assert not medium >= high
+
+        assert medium >= medium2
+        assert high >= low
+        assert not low >= high
+
+        assert low >= low2
+        assert medium >= low
+        assert not low >= medium
+
+
+    def test_get_metadata_and_deserialize(self):
+        dummy_event = DummyEvent().init({"prop_1": 1, "prop_2": "hello"})
+
+        raw_dict = dummy_event.get_metadata()
+
+        deserialized = Event.deserialize(raw_dict)
+
+        assert dummy_event == deserialized
+
+    def test_deserialize_no_event_type(self):
+        with self.assertRaises(Exception):
+            Event.deserialize({})
+
+    def test_deserialize_event_type_does_not_exist(self):
+        with self.assertRaises(Exception):
+            Event.deserialize({"event_type": "__this_event_type_does_not_exist"})
+
+    def test_network_propagate(self):
+        new_event = FakeA().init({"key": 1})
+        assert not new_event._network_propagate
+        new_event_instance = new_event.network_propagate()
+        assert new_event._network_propagate
+        assert new_event_instance == new_event # verify functional programming works
+
+
+    def test_str(self):
+        
+        fake_a = FakeA()
+        fake_a_str = str(fake_a)
+
+        assert FakeA.__name__ in fake_a_str
+
 
 
 class FakeA(Event):
@@ -85,6 +189,23 @@ class FakeA(Event):
 class FakeB(Event):
     def __init__(self):
         super().__init__(priority=10, schema=Schema({}))
+
+class LowPriority(Event):
+    def __init__(self):
+        super().__init__(priority=100, schema=Schema({}))
+
+class MediumPriority(Event):
+    def __init__(self):
+        super().__init__(priority=50, schema=Schema({}))
+
+class HighPriority(Event):
+    def __init__(self):
+        super().__init__(priority=0, schema=Schema({}))
+
+
+class LowPriority(Event):
+    def __init__(self):
+        super().__init__(priority=100, schema=Schema({}))
 
 class DummyEvent(Event):
     def __init__(self):
